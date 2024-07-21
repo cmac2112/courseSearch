7fb308069c7265eb02fbbd78cfe35e2c3111317d
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-07-20 21:48:34 -0500

    maps, api fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,53 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import ApiExample from "./components/ApiExample";
+import TestRoute from "./components/TestRoute";
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [string, setString] = React.useState("");
+import LeafletTest from "./components/Leaflet/LeafletTest";
 
-  const getHello = async () =>{
-    try{
-    const response = await fetch("http://127.0.0.1:5000/helloworld")
-    const data = await response.json()
-    console.log(data.message)
-    setString(data.message)
-    }catch(err){
-      console.log(err)
-    }
+//figure out map stuff, preferrably a png or actualy maps depending on if they free
+//need sidebar component
+//need footer component
 
-  }
-  useEffect(() =>{
-    getHello()
-  }, [])
+
+//get coordinates for each room
+//figure out routes
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div>test
-        {string}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/courseSearch/" element={<ApiExample />} />
+        <Route path="/courseSearch/test" element={<TestRoute />} />
+        <Route path="/courseSearch/map" element={<LeafletTest />} />
+      </Routes>
+      </HashRouter>
+    </BrowserRouter>
+  );
+  
 }
-
-export default App
---
+++ client/src/components/TestRoute.tsx
@@ -0,0 +1,8 @@
+
+const TestRoute = () => {
+  return (
+    <div>TestRoute</div>
+  )
+}
+
+export default TestRoute
